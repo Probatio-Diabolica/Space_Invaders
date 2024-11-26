3a64e3d19a56068b59578b8e03f73ef3c2a3a805
Probatio-Daibolica <aayushkaintura9481@gmail.com>
2024-11-26 19:28:27 +0530

    commented this until further improvements

--- src/Utils/EnemySegment.hpp
+++ src/Utils/EnemySegment.hpp
@@ -1,24 +1,24 @@
-#pragma once
-
-template<typename temp>
-class enemySegment
-{
-private:
-	temp *m_array;
-	short int m_top = -1;
-	short int m_size;
-	//int path;
-public:
-	bool mirror;
-private:
-
-public:
-	enemySegment();
-	enemySegment(short int size);
-	enemySegment(const enemySegment& e);
-	void render() const ;
-	void updates();
-	temp& operator[](int index) const;
-};
-
-
+//#pragma once
+//
+//template<typename temp>
+//class enemySegment
+//{
+//private:
+//	temp *m_array;
+//	short int m_top = -1;
+//	short int m_size;
+//	//int path;
+//public:
+//	bool mirror;
+//private:
+//
+//public:
+//	enemySegment();
+//	enemySegment(short int size);
+//	enemySegment(const enemySegment& e);
+//	void render() const ;
+//	void updates();
+//	temp& operator[](int index) const;
+//};
+//
+//
